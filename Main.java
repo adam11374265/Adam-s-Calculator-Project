2919d8aaf46c9745358621ebc6166989aced170f
Adam Azcorra <adam.azcorra@acecharterhigh.org>
2019-10-18 10:27:28 -0700

    Modified main loop.

--- Main.java
+++ Main.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
+
 class Main
 {
   public static void main(String[] args)
   {
     // Global variables.
     Scanner _scanner = new Scanner(System.in);
-    String result;
-    // Tells user how to end program
-    System.out.println("Type 'end' to end program.");
-    System.out.println(" ");
+    String _userInput;
+
+    // Print start message.
+    System.out.println("At the prompt, type in input.\n");
+
     // loop until user wants to quit.
     while(true)
     {
       // Ask the user for an input.
-      System.out.print("Enter equation: ");
-      result = _scanner.nextLine();
-      // Stops program.
-      if(result.equals("End") || result.equals("end"))
-      {
-        break;
-      }
+      System.out.print("INPUT> ");
+      _userInput = _scanner.nextLine();
 
+      // Print out result.
+      System.out.println("\nRESULT> " + _userInput + "\n");
     }
   }
 }
