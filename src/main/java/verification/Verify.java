57308a75c01d7b5c04b91d520e033104837f027b
Adam Azcorra <adam.azcorra@acecharterhigh.org>
2019-10-22 16:59:33 -0700

    Check if string has valid characters

---
+++ src/main/java/verification/Verify.java
@@ -0,0 +1,34 @@
+class Verify
+{
+  // .*[].*
+  public static boolean invalidChars(String a)
+  {
+    // Checks if string contains invalid characters.
+    if(a.contains(  .*["abcdefghijklmnopqrstuvwxyzABCCEDGHIJKLMNOPQRSTUVWXYZ&$#@!~`\|/?.>,<="))
+    {
+      // Tells user to re enter expression.
+      System.out.print("Invalid expression. Re enter.");
+    }
+
+  }
+  public staic boolean balancedParenthesis(String a)
+  {
+    // create stack for '('.
+    stack<char> p;
+    // Loop through the string.
+    for(int i = 0; i < a.length(); i++)
+    {
+      if(a.charAt(i) == '(')
+      {
+        p.push(a.charAt(i));
+      }
+      if(a.charAt(i) == ')')
+      {
+        if(p.top() == '(')
+        {
+
+        }
+      }
+    }
+  }
+}
