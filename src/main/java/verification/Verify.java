ce8a340eeab48e8d8d73ae1e8c75209ac2b001d1
Adam Azcorra <adam.azcorra@acecharterhigh.org>
2019-10-28 13:05:19 -0700

    Fixed errors

--- src/main/java/verification/Verify.java
+++ src/main/java/verification/Verify.java
@@ -5,17 +5,17 @@ class Verify
   public static boolean invalidChars(String a)
   {
     // Checks if string contains invalid characters.
-    if(a.contains(  .*["abcdefghijklmnopqrstuvwxyzABCCEDGHIJKLMNOPQRSTUVWXYZ&$#@!~`\|/?.>,<="))
+    if(a.contains(".*[abcdefghij\"k\'lmnopqrstuvwxyzABCCEDGHIJKLMNOPQRSTUVWXYZ&$#@!~`\\\\|/?.>,<[\\]{\\}]=)].*"))
     {
       // Tells user to re enter expression.
       System.out.print("Invalid expression. Re enter.");
     }
 
   }
-  public staic boolean balancedParenthesis(String a)
+  public static boolean balancedParenthesis(String a)
   {
     // create stack for '('.
-    stack<char> p;
+    Stack<Character> p = new Stack<Character>();
     // Loop through the string.
     for(int i = 0; i < a.length(); i++)
     {
@@ -26,10 +26,6 @@ class Verify
       else if(a.charAt(i) == ')')
       {
         p.pop();
-      }
-      else
-      {
-        continue;
       }
     }
 
