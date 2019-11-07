bda526c120c4a02aae10ef61b2163e57b49e3a59
adam11374265 <47756151+adam11374265@users.noreply.github.com>
2019-11-07 12:55:23 -0800

    Changed or to and on line 23 to fix invalidChar
    Changed or to and on line 23 to fix invalidChars method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ class Main
       _userInput = _scanner.nextLine();
 
       // Checks for balanced parenthesis and invalid characters.
-      if(Verify.invalidChars(_userInput) == true || Verify.balancedParenthesis(_userInput) == true)
+      if(Verify.invalidChars(_userInput) == true && Verify.balancedParenthesis(_userInput) == true)
       {
         System.out.println("Invalid input. Re-enter input.");
       }
