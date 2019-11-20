d3837b4a837353b9ea6466c062ede739a80dc375
Adam Azcorra <adam.azcorra@acecharterhigh.org>
2019-11-19 16:55:00 -0800

    started evaluation method.

---
+++ src/main/java/evaluation.java
@@ -0,0 +1,11 @@
+class evaluation
+{
+  public static boolean precedence(char a, char b)
+  {
+    Hashmap<Character, Ineger> precedence = new HashMap<>();
+  }
+  public static String infixPostfixConversion(String a)
+  {
+    
+  }
+}
--- src/main/java/verification/Verify.java
+++ src/main/java/verification/Verify.java
@@ -8,18 +8,15 @@ public class Verify
   public static boolean invalidChars(String a)
   {
     // Checks if string contains invalid characters.
-<<<<<<< HEAD
-    return a.matches(".*[abcdefghij\"k\'lmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&$#@!~`\\\\|/?.>,<[\\]{\\}]=)].*");
-=======
-    return a.contains(".*[abcdefghij\"k\'lmnopqrstuvwxyzABCCEDGHIJKLMNOPQRSTUVWXYZ&$#@!~`\\\\|/?.>,<[\\]{\\}]=)].*");
-    
->>>>>>> 0faeec1ab2141ddcf910b4b40a3bd55c36977901
+
+    return a.matches(".*[abcdefghij\"k\'lmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ&$#@!~`\\\\|/?>,<[\\]{\\}]=].*");
+
   }
-  public static boolean balancedParenthesis(String a)
+  public static boolean imbalancedParenthesis(String a)
   {
     // Create stack for '('.
     Stack<Character> stack = new Stack<Character>();
-    
+
     for(int i = 0; i < a.length(); i++)
     {
       // If scanned char is '(' then push scanned char to the stack.
@@ -27,46 +24,27 @@ public class Verify
       {
         stack.push(a.charAt(i));
       }
-<<<<<<< HEAD
-      // If scanned char is equal to ')', and the stack is not empty then take a char off the stack.
-      else if(a.charAt(i) == ')' && !p.empty())
-=======
-      else if(a.charAt(i) == ')' && stack.empty())
->>>>>>> 0faeec1ab2141ddcf910b4b40a3bd55c36977901
-      {
-        return false;
-      }
-<<<<<<< HEAD
-      else if(a.charAt(i) == ')' && p.empty())
-      {
-        return true;
-      }
-
-    }
-    // If the stack is empty then return false that the string is valid.
-    return !p.empty();
-
-
-  }
-=======
       else if(a.charAt(i) == ')' && !stack.empty())
       {
         stack.pop();
       }
-      
+        else if(a.charAt(i) == ')' && stack.empty())
+      {
+        return true;
+      }
     }
-    
-    return stack.empty();
-   
->>>>>>> 0faeec1ab2141ddcf910b4b40a3bd55c36977901
 
-  public static boolean syntaxErrors(String a)
+      return !stack.empty();
+
+  }
+
+  public static boolean invalidSyntax(String a)
   {
-    return !a.matches("(.*[\\d]\\(.*)|(.*\\)[\\d].*)|(.*[-+\\*\\/^][+\\*\\/^\\)].*)|(.*\\.[\\d]+\\..*)|(.*^0{2,}\\..*)|(.*\\(\\).*)");
+    return a.matches("(.*[\\d]+\\(.*)|(.*\\)[\\d].*)|(.*[-+\\*\\/^][+\\*\\/^\\)].*)|(.*\\.[\\d]*\\..*)|(.*^0{2,}\\..*)|(.*\\(\\).*)");
   }
 
   public static boolean checkValid(String a)
   {
-    return invalidChars(a) && balancedParenthesis(a) && syntaxErrors(a);
+    return invalidSyntax(a) || invalidChars(a) || imbalancedParenthesis(a);
   }
 }
