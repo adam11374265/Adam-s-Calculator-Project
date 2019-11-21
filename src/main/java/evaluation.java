11a20ef43db35d6df9b454eb87d51940fa966679
adam11374265 <47756151+adam11374265@users.noreply.github.com>
2019-11-21 06:13:47 -0800

    Update evaluation.java
    made tokenizior and precedence method. Started Infix to operator method.

--- src/main/java/evaluation.java
+++ src/main/java/evaluation.java
@@ -1,11 +1,41 @@
 class evaluation
 {
-  public static boolean precedence(char a, char b)
+  public static String tokenizor(String a)
+    {
+      a.replace(" ","");
+      if(a.matches("((?<=[+\\-*\\^\\/])\\-?[\\.\\d]+)|([\\.\\d]+)|([+\\-*\\/^\\(\\)])"))
+      {
+        return a.replace("", " "); 
+      }
+      else
+      {
+        return a;
+      }
+    }
+
+  public static int precedence(char a)
   {
-    Hashmap<Character, Ineger> precedence = new HashMap<>();
+    switch(a)
+    {
+      case '+':
+      case '-':
+        return 1;
+      case '/':
+      case '*':
+        return 2;
+      case '^':
+        return 3;
+    }
+    return 0;
   }
-  public static String infixPostfixConversion(String a)
+
+  public static String infixPostfixConverter(String a)
   {
-    
+    String answer;
+    Stack<Character> Operators = new Stack<>();
+    for(int i = 0; i < a.length(); i++)
+    {
+      
+    }
   }
 }
