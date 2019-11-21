a8ee34b3fd8b5972fb1f6647d14d310971f35d99
adam11374265 <47756151+adam11374265@users.noreply.github.com>
2019-11-21 12:50:26 -0800

    Update evaluation.java
    Finished Infix to Postfix converter method.

--- src/main/java/evaluation.java
+++ src/main/java/evaluation.java
@@ -35,7 +35,39 @@ class evaluation
     Stack<Character> Operators = new Stack<>();
     for(int i = 0; i < a.length(); i++)
     {
-      
+      if(charAt(i).matches("[\\d]"))
+      {
+        answer = answer + charAt(i);
+      }
+      else if(charAt(i).matches("\*\+\-\/\^\(") && Operators.empty())
+      {
+        Operators.push(charAt(i));
+      }
+      else if(charAt(i).matches("\*\+\-\/\^") && precedence(charAt(i)) >= precedence(charAt(Operators.peek)))
+      {
+        Operators.push(charAt(i));
+      }
+      else if(charAt(i).matches("\*\+\-\/\^")  && precedence(charAt(i)) <= precedence(charAt(Operators.peek)))
+      {
+        Operators.push(charAt(i));
+        while(precedence(charAt(i)) <= precedence(charAt(Operators.peek)) || !Operators.empty())
+        {
+          answer = answer + Operators.pop();
+        }
+      }
+     else if(charAt(i) == ')')
+     {
+      while(!charAt(i).equals('('))
+      {
+        answer = answer + Operators.pop();
+      }
+       Operators.pop()
+     }
     }
+    if(!Operators.empty())
+    {
+      answer = answer + Operators.pop();
+    }
+    return answer;
   }
 }
