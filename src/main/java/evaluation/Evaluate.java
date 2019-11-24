03e817531749312837a69a5bbdf1651da25198b7
adam11374265 <47756151+adam11374265@users.noreply.github.com>
2019-11-24 08:00:57 -0800

    Update Evaluate.java
    Fixed the infixToPostfixCoverter method so that it now works properly.

--- src/main/java/evaluation/Evaluate.java
+++ src/main/java/evaluation/Evaluate.java
@@ -1,3 +1,4 @@
+
 package evaluation;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -15,74 +16,70 @@ public class Evaluate
     return result;
   }
 
-  public static boolean precedence(String a, String b)
+  public static boolean greaterPrecedence(String a, String b)
   {
     HashMap<String, Integer> map = new HashMap<>();
     {{
-      map.put("(", -1);
+      map.put("(", 1);
       map.put("+", 1);
       map.put("-", 1);
       map.put("*", 2);
       map.put("/", 2);
       map.put("^", 3);
     }}
-    return map.get(a) < map.get(b);
+    return map.get(b) <= map.get(a);
   }
 
   public static String infixPostfixConverter(String c)
   {
     String[] a = tokenizor(c);
-    String answer = "";
+    String result = "";
     Stack<String> Operators = new Stack<>();
     for(int i = 0; i < a.length; i++)
     {
-      // If scanned index of array is a number, then add it to the answer.
-      if(a[i].equals("[\\d]"))
+      if(a[i].matches(".*[\\d].*"))
       {
-        answer = answer + a[i];
+        result = result + a[i];
       }
-      // If scanned index of array is an operator and Operator stack is empty
-      // then add it to the stack.
-      else if(a[i].equals("\\*\\+\\-\\/\\^\\(") && Operators.empty())
+      
+      else if(a[i].matches(".*[-+\\*/^].*"))
       {
-        Operators.push(a[i]);
-      }
-      // If scanned index is an operator and if the precedence of the scanned index
-      // is greater than the top of the stack then push it to the Operator stack.
-      else if(a[i].equals("\\*\\+\\-\\/\\^") && precedence(a[i], Operators.peek()))
-      {
-        Operators.push(a[i]);
-      }
-      // If scanned index is an operator and the precedence of the index is less than 
-      // the top of the stack.
-      else if(a[i].equals("\\*\\+\\-\\/\\^")  && !precedence(a[i], Operators.peek()))
-      {
-        // While the precedence of the scanned index is less than the top of the stack 
-        // and the stack is not empty, then add the top of the stack to the answer. 
-        while(!precedence(a[i], Operators.peek()) || !Operators.empty())
+        if(Operators.empty())
         {
-          answer = answer + Operators.pop();
+          Operators.push(a[i]);
         }
-        // Push the scanned index to the stack.
+        
+        else
+        {
+          while(!Operators.empty() && greaterPrecedence(Operators.peek(), a[i]))
+          {
+            result = result + " " + Operators.pop();
+          }
+
+          Operators.push(a[i]);
+        }
+      } 
+
+      else if(a[i] == "(")
+      {
         Operators.push(a[i]);
       }
-      // If a ')' is scanned then, while a '(' is not scanned add the top of the stack to the answer.
-      else if(a[i].equals(")"))
+
+      else if(a[i] == ")")
       {
-        while(!a[i].equals("("))
+        while(!Operators.peek().equals("("))
         {
-          answer = answer + Operators.pop();
+          result = result + " " + Operators.pop();
         }
-        // Take the '(' off of the stack.
+
         Operators.pop();
       }
     }
-    // If the stack is not empty, then add the stack to the answer.
     if(!Operators.empty())
     {
-      answer = answer + Operators.pop();
+      result = result + " " + Operators.pop();
     }
-    // Output the answer.
-    return answer;
+
+    return result;
   }
 }
