87a202a7b95277fce2b5e62b58186837eef9b0b9
adam11374265 <47756151+adam11374265@users.noreply.github.com>
2019-11-26 08:53:58 -0800

    Update Evaluate.java
    Fixed buggs so that infixtopostfix converter method return the proper output for "9*9+9", "9*(8-8)*9", and "9--9".

--- src/main/java/evaluation/Evaluate.java
+++ src/main/java/evaluation/Evaluate.java
@@ -1,85 +1,87 @@
-
 package evaluation;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import evaluation.Evaluate;
+ 
 public class Evaluate
 {
-  public static String[] tokenizor(String a)
-  {
-    a.replace(" ","");
-    final Pattern pattern = Pattern.compile("((?<=[+-\\/\\^*])\\-?[\\d\\.]+)|([+-\\/\\^*\\)\\(])|([\\d\\.]+)", Pattern.MULTILINE);
-    final Matcher matcher = pattern.matcher(a);
-    final String[] result = matcher.replaceAll("$0 ").split("\\s");
+ public static String[] tokenizor(String a)
+ {
+   a.replace(" ","");
+   final Pattern pattern = Pattern.compile("((?<=[+-\\/\\^*])\\-?[\\d\\.]+)|([+-\\/\\^*\\)\\(])|([\\d\\.]+)", Pattern.MULTILINE);
+   final Matcher matcher = pattern.matcher(a);
+   final String[] result = matcher.replaceAll("$0 ").split("\\s");
+ 
+   return result;
+ }
+ 
+ public static boolean greaterPrecedence(String a, String b)
+ {
+   HashMap<String, Integer> map = new HashMap<String, Integer>();
+   {{
+     map.put("(", 0);
+     map.put("+", 1);
+     map.put("-", 1);
+     map.put("*", 2);
+     map.put("/", 2);
+     map.put("^", 3);
+   }}
+   return map.get(a) >= map.get(b);
+ }
+ 
+ public static String infixPostfixConverter(String c)
+ {
+   String[] a = tokenizor(c);
+   String result = "";
+   Stack<String> Operators = new Stack<String>();
+   for(int i = 0; i < a.length; i++)
+   {
+     if(a[i].matches(".*[\\d].*"))
+     {
+       result = result + a[i] + " ";
+     }
+     else if(a[i] == "(")
+     {
+       Operators.push(a[i]);
+     }
 
-    return result;
-  }
-
-  public static boolean greaterPrecedence(String a, String b)
-  {
-    HashMap<String, Integer> map = new HashMap<>();
-    {{
-      map.put("(", 1);
-      map.put("+", 1);
-      map.put("-", 1);
-      map.put("*", 2);
-      map.put("/", 2);
-      map.put("^", 3);
-    }}
-    return map.get(b) <= map.get(a);
-  }
-
-  public static String infixPostfixConverter(String c)
-  {
-    String[] a = tokenizor(c);
-    String result = "";
-    Stack<String> Operators = new Stack<>();
-    for(int i = 0; i < a.length; i++)
-    {
-      if(a[i].matches(".*[\\d].*"))
-      {
-        result = result + a[i];
-      }
-      
-      else if(a[i].matches(".*[-+\\*/^].*"))
-      {
-        if(Operators.empty())
-        {
-          Operators.push(a[i]);
-        }
-        
-        else
-        {
-          while(!Operators.empty() && greaterPrecedence(Operators.peek(), a[i]))
+     else if(a[i].matches(".*[-\\+*\\/^].*"))
+     {
+       if(!Operators.empty())
+       {
+          while(!Operators.empty() && !a[i].equals("(") && greaterPrecedence(Operators.peek(), a[i]))
           {
-            result = result + " " + Operators.pop();
+            result = result + Operators.pop() + " ";
           }
 
-          Operators.push(a[i]);
-        }
-      } 
+         Operators.push(a[i]);
+       }
 
-      else if(a[i] == "(")
+       else if(Operators.empty())
+       {
+         Operators.push(a[i]);
+       }
+     }
+
+     else if(a[i] == ")")
+     {
+       while(!Operators.peek().equals("("))
       {
-        Operators.push(a[i]);
+        result = result + " " + Operators.pop();
       }
+      Operators.pop();
+     }
+   }
+ 
+   while(!Operators.empty())
+   {
+     result = result + " " + Operators.pop();
+   }
 
-      else if(a[i] == ")")
-      {
-        while(!Operators.peek().equals("("))
-        {
-          result = result + " " + Operators.pop();
-        }
+   return result;
 
-        Operators.pop();
-      }
-    }
-    if(!Operators.empty())
-    {
-      result = result + " " + Operators.pop();
-    }
-
-    return result;
   }
 }
+ 
+
